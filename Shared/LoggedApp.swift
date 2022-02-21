67f0ceac586773f46a1c7ba5b35878d51ca579d4
LuckDuracell <lukedru@yahoo.com>
2022-02-21 10:54:53 -0800

    Initial Commit

---
+++ Shared/LoggedApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoggedApp.swift
+//  Shared
+//
+//  Created by Luke Drushell on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct LoggedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
